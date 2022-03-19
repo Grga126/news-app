36914de56f4be2c287a93144943873d6e1192864
Grga126 <milan.grgic126@gmail.com>
2022-03-19 14:37:30 +0100

    Starting login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-   <>
-   </>
+    <div>
+    <h1 class="header">Dobrodosli na vecernje vesti</h1>
+    
+    <div class="div1">
+        <p class="paragraph">Unesite Vas email</p>
+        <input class="input" type="text" id="Email" name="Email"/><br/>
+
+        <p class="paragraph">Unesite Vasu lozinku</p>
+        <input class="input" type="password" id="password" name="password"/><br/>
+
+        <button type="button">Prijava</button>
+    </div>
+
+    <a class="links" href="sign.html">Otvorite nalog!</a>
+    <a class="links" href="login.html">Zaboravili ste sifru?</a>
+   </div>
   );
 }
 
