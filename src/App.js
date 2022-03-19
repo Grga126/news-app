bbcf25002790be21daf051bf1ff67071663db878
Grga126 <milan.grgic126@gmail.com>
2022-03-19 15:06:17 +0100

    Ovde je uradjen login i register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import './App.css';
+import Login from "./html/login";
+
+import {  BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    <h1 class="header">Dobrodosli na vecernje vesti</h1>
-    
-    <div class="div1">
-        <p class="paragraph">Unesite Vas email</p>
-        <input class="input" type="text" id="Email" name="Email"/><br/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
 
-        <p class="paragraph">Unesite Vasu lozinku</p>
-        <input class="input" type="password" id="password" name="password"/><br/>
-
-        <button type="button">Prijava</button>
-    </div>
-
-    <a class="links" href="sign.html">Otvorite nalog!</a>
-    <a class="links" href="login.html">Zaboravili ste sifru?</a>
-   </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
