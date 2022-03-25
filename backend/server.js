528e94df36dadf5f7f3a212556380e3efcdb3f4e
milan.marjanov <milan.marjanov@pbssolutions.net>
2022-03-25 22:31:31 +0100

    Implement error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 var app = express();
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("")
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
