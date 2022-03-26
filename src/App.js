6bbbde8b286aa8bb4bf6d6184fc3f13b7c394061
milan.marjanov <milan.marjanov@pbssolutions.net>
2022-03-26 14:18:48 +0100

    Dashboard init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
+import "./App.css";
 import Login from "./html/login";
 import Signin from "./html/signin";
-import Nav from './html/nav';
-import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from "./components/nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signin" element={<Signin/>}/>
-        <Route path="/nav" element={<Nav/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/nav" element={<Nav />} />
       </Routes>
     </BrowserRouter>
   );
--- src/html/login.js
+++ src/html/login.js
@@ -1,18 +1,24 @@
-export default function Login(){
-    return (
-        <div>
-        <h1 className="header">DOBRODOSLI NA VECERNJE VESTI</h1>
-        <form className="form">
-            <article className="articleinput">
-                <p>Unesite e-mail:</p>
-                <input className="input" type="text" id="email" name="email"/><br/>
-                <p>Unesite sifru:</p>
-                <input type="password" id="password" name="password"/><br/>
-            </article>
-        </form>
-            <button className="button">Login</button>
-            <a className="link" href="signin">Nemate nalog?</a>
-        </div>
-    )
-}
+import Nav from "../components/nav";
 
+export default function Login() {
+  return (
+    <div>
+      <Nav />
+      <h1 className="header">DOBRODOSLI NA VECERNJE VESTI</h1>
+      <form className="form">
+        <article className="articleinput">
+          <p>Unesite e-mail:</p>
+          <input className="input" type="text" id="email" name="email" />
+          <br />
+          <p>Unesite sifru:</p>
+          <input type="password" id="password" name="password" />
+          <br />
+        </article>
+      </form>
+      <button className="button">Login</button>
+      <a className="link" href="signin">
+        Nemate nalog?
+      </a>
+    </div>
+  );
+}
