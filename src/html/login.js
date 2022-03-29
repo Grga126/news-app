5fac5369c81863885d290b239d26d01549bf31dc
Grga126 <milan.grgic126@gmail.com>
2022-03-29 20:29:37 +0200

    fixing login funcion

--- src/html/login.js
+++ src/html/login.js
@@ -1,17 +1,17 @@
 export default function Login(){
     return (
         <div className="login-page background">
-        <h1 className="header">DOBRODOSLI NA VECERNJE VESTI</h1>
-        <form className="form">
-            <article className="articleinput">
+        <h1 className="login-page-header">DOBRODOSLI NA VECERNJE VESTI</h1>
+        <form className="login-page-form">
+            <article className="login-page-articleinput">
                 <p>Unesite e-mail:</p>
-                <input className="input" type="text" id="email" name="email"/><br/>
+                <input className="login-page-input" type="text" id="email" name="email"/><br/>
                 <p>Unesite sifru:</p>
-                <input type="password" id="password" name="password"/><br/>
+                <input className="login-page-input" type="password" id="password" name="password"/><br/>
             </article>
         </form>
-            <button className="button">Login</button>
-            <a className="link" href="signin">Nemate nalog?</a>
+            <button className="login-page-button">Login</button>
+            <a className="login-page-link" href="signin">Nemate nalog?</a>
         </div>
     )
 }
